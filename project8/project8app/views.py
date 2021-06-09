1f0e7f0ea95c6038847ca60bfd1d17e952e42538
pevin john <82257974+pevin06@users.noreply.github.com>
2021-06-09 09:57:49 +0530

    Update views.py

--- project8/project8app/views.py
+++ project8/project8app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from . models import Place
+from . models import New
 # Create your views here.
 def demo(request):
-    return render(request,"index.html")
+    obj = Place.objects.all()
+    sub = New.objects.all()
+    return render(request,"index.html",{'result':obj,'any':sub})
+
