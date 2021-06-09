391329a21eef7820e9aa1a8c31837b43cef6beca
pevin john <82257974+pevin06@users.noreply.github.com>
2021-06-09 09:57:09 +0530

    Update models.py

--- project8/project8app/models.py
+++ project8/project8app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    name=models.CharField(max_length=250)
+    img=models.ImageField(upload_to='pics')
+    desc=models.TextField()
+    def __str__(self):
+        return self.name
+class New(models.Model):
+    title=models.CharField(max_length=50)
+    imag=models.ImageField(upload_to='pic')
+    des=models.TextField()
+    def __str__(self):
+        return self.title
